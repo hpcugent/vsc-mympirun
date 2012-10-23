7ee2b0efc173894461a9d511404b18049a437010
Jens Timmerman <jens.timmerman@ugent.be>
2012-10-23 16:28:39 +0200

    fixed typos in license header

--- bin/mympirun.py
+++ bin/mympirun.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ##
-# Copyright 2011-2012 Stijnd De Weirdt
+# Copyright 2009-2012 Stijn De Weirdt
 #
 # This file is part of VSC-tools,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
