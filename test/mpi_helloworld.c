3928d95f9dca8e7f2336556dd998855e8cfabd79
jerodcle <jeroen.declerck@ugent.be>
2016-07-05 09:22:29 +0200

    tests

--- test/mpi_helloworld.c
+++ test/mpi_helloworld.c
@@ -3,16 +3,14 @@
 #include <mpi.h>
 
 
-int main (argc, argv)
-     int argc;
-     char *argv[];
+main(int argc, char **argv)
 {
-  int rank, size;
+   int node;
 
-  MPI_Init (&argc, &argv);  /* starts MPI */
-  MPI_Comm_rank (MPI_COMM_WORLD, &rank);  /* get current process id */
-  MPI_Comm_size (MPI_COMM_WORLD, &size);  /* get number of processes */
-  printf( "Hello world from process %d of %d\n", rank, size );
-  MPI_Finalize();
-  return 0;
+   MPI_Init(&argc,&argv);
+   MPI_Comm_rank(MPI_COMM_WORLD, &node);
+
+   printf("Hello World from Node %d\n",node);
+
+   MPI_Finalize();
 }
