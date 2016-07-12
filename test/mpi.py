cc674f5ad72df0a73f60279e582cb72c93dfa8be
jerodcle <jeroen.declerck@ugent.be>
2016-07-12 11:03:28 +0200

    unfinished tests before restructuring

--- test/mpi.py
+++ test/mpi.py
@@ -29,7 +29,7 @@ Tests for the vsc.utils.missing module.
 """
 import os
 import stat
-from unittest import TestCase
+import unittest
 from random import choice
 from string import ascii_uppercase
 
@@ -37,18 +37,22 @@ from vsc.utils.fancylogger import getLogger
 from vsc.utils.run import run_simple
 from vsc.mympirun.rm.factory import getinstance
 import vsc.mympirun.mpi.mpi as mpim
+import vsc.mympirun.mpi.intelmpi as intelmpi
+import vsc.mympirun.mpi.openmpi as openmpi
+import vsc.mympirun.mpi.mpich as mpich
 from vsc.mympirun.rm.local import Local
 from vsc.mympirun.option import MympirunOption
 
 # we wish to use the mpirun we ship
 os.environ["PATH"] = os.path.dirname(os.path.realpath(__file__)) + os.pathsep + os.environ["PATH"]
 
-_logger = getLogger()
+_testlogger = getLogger()
 
+class TestMPI(unittest.TestCase):
 
-class TestMPI(TestCase):
-
-    """Test for the mpi class."""
+    #######################
+    ## general functions ##
+    #######################
 
     def test_whatMPI(self):
         scriptnames = ["ompirun", "mpirun", "impirun", "mympirun"]
@@ -56,17 +60,17 @@ class TestMPI(TestCase):
             # if the scriptname is an executable located on this machine
             if mpim.which(scriptname):
                 (returned_scriptname, mpi, found) = mpim.whatMPI(scriptname)
-                _logger.debug("%s, %s, %s", returned_scriptname, mpi, found)
+                _testlogger.debug("%s, %s, %s", returned_scriptname, mpi, found)
                 # if an mpi implementation was found
                 if mpi:
                     self.assertTrue(mpi in found)
                     self.assertTrue(returned_scriptname == scriptname)
                 else:
-                    self.assertEqual(returned_scriptname, "mpirun")
+                    self.assertEqual(returned_scriptname, "mpirun", "")
 
     def test_stripfake(self):
         """Test if stripfake actually removes the /bin/fake path in $PATH"""
-        _logger.debug("old path: %s", os.environ["PATH"])
+        _testlogger.debug("old path: %s", os.environ["PATH"])
         mpim.stripfake()
         newpath = os.environ["PATH"]
         self.assertFalse(("bin/%s" % mpim.FAKE_SUBDIRECTORY_NAME) in newpath)
@@ -76,7 +80,12 @@ class TestMPI(TestCase):
         for scriptname in scriptnames:
             mpimwhich = mpim.which(scriptname) +"\n"
             ec, unixwhich = run_simple("which " + scriptname)
-            self.assertEqual(mpimwhich, unixwhich)
+            self.assertEqual(mpimwhich, unixwhich, msg=("the return values of unix which and which() aren't the same: "
+                "%s != %s") % (mpimwhich, unixwhich))
+
+    ###################
+    ## MPI functions ##
+    ###################
 
     def test_options(self):
         """Bad options"""
@@ -92,13 +101,43 @@ class TestMPI(TestCase):
         optdict = mpi_instance.options
 
         # why isnt this a dict??
-        _logger.debug("MPI INSTANCE OPTIONS: %s, type %s", optdict, type(optdict))
+        _testlogger.debug("MPI INSTANCE OPTIONS: %s, type %s", optdict, type(optdict))
 
         # for opt in m.args:
         #    self.assertFalse(opt in mpi_instance.options)
+        _testlogger.debug("11 HALLO")
+
+    def test_is_mpirun_for(self):
+        _testlogger.debug("21 HALLO %s" , "HALLO")
+        m = MympirunOption()
+        _testlogger.debug("22 HALLO")
+        ompi_instance = getinstance(openmpi.OpenMPI, Local, m)
+        _testlogger.debug("23 HALLO")
+        asdf = ompi_instance._mpiscriptname_for[0]
+        _testlogger.debug("IS MPIRUN FOR ASDf %s:", asdf)
+        path = mpim.which(asdf)
+        _testlogger.debug("IS MPIRUN FOR PATH %s:", path)
+        res = ompi_instance._is_mpirun_for(path)
+        _testlogger.debug("IS MPIRUN FOR RES %s:", res)
+
+    def test_set_omp_threads(self):
+        _testlogger.debug("31 HALLO")
+        m = MympirunOption()
+        _testlogger.debug("32 HALLO")
+        mpi_instance = getinstance(mpim.MPI, Local, m)
+        _testlogger.debug("33 HALLO")
+        mpi_instance.set_omp_threads()
+        _testlogger.debug("34 HALLO")
+        self.assertTrue(getattr(mpi_instance.options, 'ompthreads', None) is not None)
+        self.assertEqual(os.environ["OMP_NUM_THREADS"],getattr(mpi_instance.options, 'ompthreads', None))
+
+#    def test_is_mpirun_for(self):
+#        m = MympirunOption()
+#        mpi_instance = getinstance(mpim.MPI, Local, m)
+#        mpi_instance.set_netmask()
 
     def test_MPI_local(self):
-        """"Test the MPI class with the local scheduler"""
+        """Test the MPI class with the local scheduler"""
         # options
         m = MympirunOption()
         mpi_instance = getinstance(mpim.MPI, Local, m)
