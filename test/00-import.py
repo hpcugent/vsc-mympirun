db94a7349b093cc4f3f1e8caa86ac6edb27d9098
Ward Poelmans <wpoely86@gmail.com>
2019-05-20 14:38:04 +0200

    Drop duplicate headers

--- test/00-import.py
+++ test/00-import.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2019-2019 Ghent University
+# Copyright 2016-2019 Ghent University
 #
 # This file is part of vsc-mympirun,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -22,28 +22,5 @@
 # You should have received a copy of the GNU General Public License
 # along with vsc-mympirun.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-# Copyright 2016-2017 Ghent University
-#
-# This file is part of vsc-mympirun,
-# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
-# with support of Ghent University (http://ugent.be/hpc),
-# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
-# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
-# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
-#
-# https://github.com/hpcugent/vsc-mympirun
-#
-# vsc-mympirun is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
-#
-# vsc-mympirun is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with vsc-mympirun.  If not, see <http://www.gnu.org/licenses/>.
-#
+""""""
 from vsc.install.commontest import CommonTest
