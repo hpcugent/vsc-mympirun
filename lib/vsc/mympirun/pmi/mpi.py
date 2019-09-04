a725b2f49ea1f106cf48958f6869194aba4ca2ed
stdweird <stijn.deweirdt@ugent.be>
2019-09-05 00:06:18 +0200

    initial pmi modules

---
+++ lib/vsc/mympirun/pmi/mpi.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2019-2019 Ghent University
+#
+# This file is part of vsc-mympirun,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-mympirun
+#
+# vsc-mympirun is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# vsc-mympirun is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with vsc-mympirun.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Base PMI MPI class, all actual classes should inherit from this one
+
+The role of the MPI class is very limited, mainly to provide supported PMI flavour/version
+"""
+
+from vsc.mympirun.common import MpiKlass
+
+class MPI(MpiKlass):
+    pass
+
